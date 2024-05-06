7f7334a8413dfffa8ef94636429ade0444c18b6a
Pavle <psarenac@gmail.com>
2024-05-06 16:39:27 +0200

    add step for svc acc key export, test tf with only vpc and subnet

--- deploy/kubernetes/terraform/main.tf
+++ deploy/kubernetes/terraform/main.tf
@@ -19,38 +19,10 @@ resource "google_compute_network" "vpc" {
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "t1-t2-subnet" {
-  name          = "t1t2-subnet"
-  region        = "us-central1"
-  network       = google_compute_network.vpc.id
-  ip_cidr_range = "10.0.0.0/24"
+resource "google_compute_subnetwork" "subnetwork" {
+    name = "subnetwork"
+    region = "us-central1"
+    network = google_compute_network.vpc.id
+    ip_cidr_range = "10.0.0.0/24"
 }
 
-resource "google_service_account" "gke-sa" {
-  account_id   = "gke-service-account-id"
-  display_name = "GKE Service Account"
-}
-
-resource "google_container_cluster" "pavle-cluster" {
-  name                     = "gke-cluster"
-  location                 = "us-central1"
-  remove_default_node_pool = true
-  initial_node_count       = 1
-}
-
-resource "google_container_node_pool" "pavle_preemptible_nodes" {
-  name       = "my-node-pool"
-  location   = "us-central1"
-  cluster    = google_container_cluster.pavle-cluster.name
-  node_count = 1
-
-  node_config {
-    preemptible  = true
-    machine_type = "e2-medium"
-
-    service_account = google_service_account.gke-sa.email
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-}
