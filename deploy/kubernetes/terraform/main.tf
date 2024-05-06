fe4926d74b91876b937cd68ffd706bdb6d8f995f
Pavle <psarenac@gmail.com>
2024-05-06 16:58:48 +0200

    add cluster and node pool resources, add -out for tf plan

--- deploy/kubernetes/terraform/main.tf
+++ deploy/kubernetes/terraform/main.tf
@@ -32,8 +32,26 @@ resource "google_service_account" "gke-sa" {
 }
 
 resource "google_container_cluster" "pavle-cluster" {
-  name                     = "gke-cluster"
-  location                 = "us-central1"
+  name     = "pavle-cluster"
+  location = "us-central1"
   remove_default_node_pool = true
   initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "pavle_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.pavle-cluster.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-small"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
