3ff95247c6c6cc92045ea388dc3cc68d82e6eec2
Pavle <psarenac@gmail.com>
2024-05-20 18:41:52 +0200

    add creds to backend

--- deploy/kubernetes/terraform/backend.tf
+++ deploy/kubernetes/terraform/backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "gcs" {
-    bucket = "t2-backend"
-    prefix = "terraform/state"
-}
+    bucket      = "t2-backend"
+    prefix      = "terraform/state"
+    credentials = "/home/psarenac/actions-runner/gcp_key.json"
+  }
 }
