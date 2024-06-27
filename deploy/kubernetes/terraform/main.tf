ab9fe33fef2aef410640f3c7c473a3e1ee869811
Pavle <psarenac@gmail.com>
2024-06-27 17:06:26 +0200

    add env vars, create ingress

--- deploy/kubernetes/terraform/main.tf
+++ deploy/kubernetes/terraform/main.tf
@@ -17,13 +17,28 @@ provider "google" {
 resource "google_compute_network" "vpc" {
   name                    = "t1-t2-vpc"
   auto_create_subnetworks = false
+  routing_mode = "GLOBAL"
 }
 
-resource "google_compute_subnetwork" "subnetwork" {
+resource "google_compute_subnetwork" "dev-subnet" {
+    name = "subnetwork"
+    region = "us-east1"
+    network = google_compute_network.vpc.id
+    ip_cidr_range = "10.0.0.0/24"
+}
+
+resource "google_compute_subnetwork" "stage-subnet" {
+    name = "subnetwork"
+    region = "us-west1"
+    network = google_compute_network.vpc.id
+    ip_cidr_range = "10.0.1.0/24"
+}
+
+resource "google_compute_subnetwork" "prod-subnet" {
     name = "subnetwork"
     region = "us-central1"
     network = google_compute_network.vpc.id
-    ip_cidr_range = "10.0.0.0/24"
+    ip_cidr_range = "10.0.2.0/24"
 }
 
 resource "google_service_account" "gke-sa" {
@@ -31,18 +46,23 @@ resource "google_service_account" "gke-sa" {
   display_name = "GKE Service Account"
 }
 
-resource "google_container_cluster" "pavle-cluster" {
-  name     = "pavle-cluster"
-  location = "us-central1"
+resource "google_container_cluster" "dev-cluster" {
+  name     = "dev-cluster"
+  location = "us-east1-c"
   remove_default_node_pool = true
   initial_node_count       = 1
+  network = google_compute_network.vpc.self_link
+  subnetwork = google_compute_subnetwork.dev-subnet.self_link
 }
 
-resource "google_container_node_pool" "pavle_preemptible_nodes" {
-  name       = "my-node-pool"
-  location   = "us-central1"
-  cluster    = google_container_cluster.pavle-cluster.name
-  node_count = 1
+resource "google_container_node_pool" "dev_preemptible_nodes" {
+  name       = "dev-node-pool"
+  location   = "us-east1-c"
+  cluster    = google_container_cluster.dev-cluster.name
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
 
   node_config {
     preemptible  = true
@@ -56,12 +76,62 @@ resource "google_container_node_pool" "pavle_preemptible_nodes" {
   }
 }
 
-resource "google_project_iam_binding" "gke_sa_admin_binding" {
-  project = "t2-plan"
+resource "google_container_cluster" "stage-cluster" {
+  name     = "stage-cluster"
+  location = "us-west1-c"
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  network = google_compute_network.vpc.self_link
+  subnetwork = google_compute_subnetwork.stage-subnet.self_link
+}
 
-  role    = "roles/compute.instanceAdmin.v1"
+resource "google_container_node_pool" "stage_preemptible_nodes" {
+  name       = "stage-node-pool"
+  location   = "us-west1-c"
+  cluster    = google_container_cluster.stage-cluster.name
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
 
-  members = [
-    "serviceAccount:${google_service_account.gke-sa.email}",
-  ]
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.gke-sa.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
+resource "google_container_cluster" "prod-cluster" {
+  name     = "prod-cluster"
+  location = "us-central1-c"
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  network = google_compute_network.vpc.self_link
+  subnetwork = google_compute_subnetwork.prod-subnet.self_link
+}
+
+resource "google_container_node_pool" "prod_preemptible_nodes" {
+  name       = "prod-node-pool"
+  location   = "us-central1-c"
+  cluster    = google_container_cluster.prod-cluster.name
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.gke-sa.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
