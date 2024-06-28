f7f255d258e01826a3344f09c3d1e9133cf74c16
Pavle <psarenac@gmail.com>
2024-06-28 17:58:23 +0200

    change bucket name

--- deploy/kubernetes/terraform/backend.tf
+++ deploy/kubernetes/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket      = "t2-backend"
+    bucket      = "t2-backend-pav"
     prefix      = "terraform/state"
     credentials = "/home/psarenac/actions-runner/gcp_key.json"
   }
